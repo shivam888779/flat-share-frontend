7c256a62c104b04b906d8b02ed3695e1133982ec
Pradhuman <pradhuman.pandey@scaleupally.io>
2024-07-15 11:23:56 +0530

    updates

--- src/component/Footer/index.tsx
+++ src/component/Footer/index.tsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 const Footer = () => { 
 
     return <Box position={"fixed"}  bottom={0} width={"100%"}>
-        here is footer
+        here is footer component
     </Box>
 }
 export default Footer;
